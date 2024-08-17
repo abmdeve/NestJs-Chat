811de870899e1f227ab55435273b126eff81a369
abmdeve <tundederome@gmail.com>
2024-08-17 17:04:12 +0200

    Update the code
    Initialization project with websocket is implemented

---
+++ src/app.gateway.ts
@@ -0,0 +1,42 @@
+import {
+  WebSocketGateway,
+  MessageBody,
+  SubscribeMessage,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+// @WebSocketGateway(8001)
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`CLIENT IS CONNECTED: ${client.id}`);
+    client.emit('connection', 'Successfully connected');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`*****CLIENT IS DISCONNECTED: ${client.id}`);
+  }
+
+  @SubscribeMessage('test')
+  sendMessage(@MessageBody() data) {
+    console.log('DATA SOCKET.IO', data);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [
-    UserModule, 
-    AuthModule, 
-    ConfigModule.forRoot(
-      {
-        isGlobal: true,
-      }
-    ),
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppGateway],
 })
 export class AppModule {}
